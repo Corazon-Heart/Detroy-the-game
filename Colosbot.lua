5e83070a42d2913a87853c896654d56526cb5eea
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-03-26 22:36:16 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -546,7 +546,7 @@ pcall(function()
 								Drogar.Humanoid.Health = 0
 								Drogar.Humanoid.Health = -math.huge
 							end
-							if tick() - StartTick >= 2.5 then
+							if tick() - StartTick >= (math.random(10,20)/10) then
 								endscript = true
 							end
 						until Drogar.Parent == nil or endscript == true
