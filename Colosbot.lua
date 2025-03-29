a526bc28b36530695d5065cdcb7f8adc209fc855
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-03-29 22:50:05 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -546,7 +546,7 @@ pcall(function()
 							if Drogar.Parent ~= nil then
 								Drogar.Humanoid.Health = 0
 							end
-							if tick() - StartTick >= 3 then
+							if tick() - StartTick >= 1 then
 								endscript = true
 							end
 						until Drogar.Parent == nil or endscript == true
