8e9d714aefb98a60caa276f9af811498c3532b31
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-03-30 20:20:39 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -677,8 +677,8 @@ pcall(function()
 			end
 			local killtime = tick()
 			repeat
-				if game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart" and Drogar:FindFirstChild"HumanoidRootPart" and (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(937.6810913085938, -517.88751220703125, 1686.1224365234375)).magnitude < 450 then
-					if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(937.6810913085938, -517.88751220703125, 1686.1224365234375)).magnitude < 450 then
+				if game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart" and Drogar:FindFirstChild"HumanoidRootPart" and (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(1005.5820922851562, -238.3594970703125, 1517.7528076171875)).magnitude < 200 then
+					if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(1005.5820922851562, -238.3594970703125, 1517.7528076171875)).magnitude < 200 then
 						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Drogar.HumanoidRootPart.CFrame*CFrame.new(0,-6,7)
 					else
 						wait(3)
