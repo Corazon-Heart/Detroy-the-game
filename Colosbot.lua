4b30d4925b5df0acf1c909440facfbc05a60d20a
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-04-02 19:52:12 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -668,42 +668,44 @@ pcall(function()
 			local Drogar = nil
 			local waitdrogartick = tick()
 			while Drogar == nil do
-				task.wait()
-				game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.D, false, game)
-				game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.D, false, game)
-				if game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart" and (game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart".Position - Vector3.new(937.6810913085938, -217.88751220703125, 1686.1224365234375)).magnitude > 10 then
-					TP(Vector3.new(937.6810913085938, -217.88751220703125, 1686.1224365234375));
-				end
-				repeat wait()
-					local args = {
-						[1] = {
-							["player"] = game:GetService("Players").LocalPlayer,
-							["Object"] = workspace.Effects.NPCS:FindFirstChild("Drakonar"),
-							["Action"] = "NPC"
-						}
-					}
-
-					game:GetService("Players").LocalPlayer.Character.CharacterHandler.Input.Events.Interact:FireServer(unpack(args))
-				until game.Players.LocalPlayer.PlayerGui.ChatGui.MainFrame.Visible == true
-				for i = 1,30 do
-					local args = {
-						[1] = "Challenge The Demon Claw, Drogar."
-					}
-
-					game:GetService("Players").LocalPlayer.Character.CharacterHandler.Input.Events.DialogueEvent:FireServer(unpack(args))
-				end
 				for i,v in pairs(workspace.Alive:GetChildren()) do
 					if v.Name:find"Drogar" then
 						Drogar = v
+					end
+				end
+				task.wait()
+				if Drogar == nil then
+					game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.D, false, game)
+					game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.D, false, game)
+					if game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart" and (game.Players.LocalPlayer.Character:FindFirstChild"HumanoidRootPart".Position - Vector3.new(937.6810913085938, -217.88751220703125, 1686.1224365234375)).magnitude > 10 then
+						TP(Vector3.new(937.6810913085938, -217.88751220703125, 1686.1224365234375));
+					end
+					repeat wait()
+						local args = {
+							[1] = {
+								["player"] = game:GetService("Players").LocalPlayer,
+								["Object"] = workspace.Effects.NPCS:FindFirstChild("Drakonar"),
+								["Action"] = "NPC"
+							}
+						}
+
+						game:GetService("Players").LocalPlayer.Character.CharacterHandler.Input.Events.Interact:FireServer(unpack(args))
+					until game.Players.LocalPlayer.PlayerGui.ChatGui.MainFrame.Visible == true
+					for i = 1,30 do
+						local args = {
+							[1] = "Challenge The Demon Claw, Drogar."
+						}
+
+						game:GetService("Players").LocalPlayer.Character.CharacterHandler.Input.Events.DialogueEvent:FireServer(unpack(args))
 					end
 				end
 				if tick() - waitdrogartick >= 30 then
 					shop()
 				end
 			end
-			wait(.3)
+			task.wait()
 			game:GetService("Players").LocalPlayer.Character.CharacterHandler.Input.Events.DialogueEvent:FireServer()
-			wait(.25)
+			task.wait(.15)
 			local rs = game:GetService("ReplicatedStorage")
 			local plr = game:GetService("Players").LocalPlayer
 			local netModule = require(rs.Modules.Network)
