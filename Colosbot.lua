c3bed6ab65cb362505d70534e4b2c684895e41a4
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-04-05 01:26:10 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -466,7 +466,7 @@ if game.PlaceId == 99995671928896 then
 		local Found = false
 		local SellItem = {
 			["Intellect Rune"] = true,
-			--["Agility Rune"] = true,
+			["Agility Rune"] = true,
 			["Spirit Rune"] = true,
 			["Stamina Rune"] = true,
 			["Strength Rune"] = true,
@@ -576,7 +576,7 @@ if game.PlaceId == 99995671928896 then
 			local sellEvent = player.Character.CharacterHandler.Input.Events.SellEvent
 			local itemsToSell = {
 				"Intellect Rune",
-				--"Agility Rune",
+				"Agility Rune",
 				"Spirit Rune",
 				"Stamina Rune",
 				"Strength Rune",
