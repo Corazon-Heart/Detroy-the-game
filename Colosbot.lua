d6dfebcd8ab4d37b94505b0c765fe733eaa7bd79
EntomaSan <138786457+EntomaSan@users.noreply.github.com>
2025-06-14 11:00:32 +0700

    Update Colosbot.lua

--- Colosbot.lua
+++ Colosbot.lua
@@ -212,30 +212,6 @@ pcall(function()
 
 		local MainTick = tick()
 		repeat wait() until CurrentCheck >= #game.Players:GetChildren() or tick() - MainTick >= 4
-		-- NoFall
-		local mt = getrawmetatable(game)
-		local oldMeta = mt.__namecall
-
-		-- Function to make metatables writeable (for bypassing the protection)
-		setreadonly(mt, false)
-
-		mt.__namecall = newcclosure(function(self, ...)
-			local method = getnamecallmethod()
-			local args = {...}
-
-			-- Check if method is "FireServer" and the arguments match our criteria
-			if method == "FireServer" and #args == 1 and typeof(args[1]) == "table" then
-				-- Check if Config is FallDamage
-				if args[1]["Config"] == "FallDamage" then
-					return false  -- Block the remote event
-				end
-			end
-
-			-- Call the original method if the condition is not met
-			return oldMeta(self, ...)
-		end)
-
-		setreadonly(mt, true)
 		local TeleportService = game:GetService("TeleportService")
 		local Players = game:GetService("Players")
 
